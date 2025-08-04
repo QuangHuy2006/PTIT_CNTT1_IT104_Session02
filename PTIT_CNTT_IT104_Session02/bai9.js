78d8a87d50745690adea504640d1c758de70e3e0
QuangHuy2006 <tonphamquanghuy3@Gmail.com>
2025-08-04 09:31:49 +0700

    Update bai9.js

--- PTIT_CNTT_IT104_Session02/bai9.js
+++ PTIT_CNTT_IT104_Session02/bai9.js
@@ -1,9 +1,42 @@
-const arr1 = [1, 2, 3, 5, 9];
-const arr2 = [4, 6, 7, 8];
-let arr3 = [];
-const combine = (arr1, arr2) => {
-  arr3 = arr3.concat(arr1, arr2);
+const getStudentSummary = ({ student }) => {
+  let { name, age, listMonhoc } = student;
+  let sum = 0;
+  let min = 10, max = 0;
+  let strongestSubject = "", weakestSubject = "";
+  for(let i of listMonhoc){
+    sum += i.score;
+    if(i.score < min){
+      min = i.score;
+      weakestSubject = i.subject;
+    }
+    if(i.score > max){
+      max = i.score;
+      strongestSubject = i.subject;
+    }
+  }
+  let avg = sum / listMonhoc.length;
+  let rank = "";
+  if(avg < 5) {
+    rank = "Học sinh yếu";
+  }else if(avg >= 5 && avg < 7) {
+    rank = "Học sinh trung bình";
+  }else if(avg >= 7 && avg < 8.5) {
+    rank = "Học sinh khá";
+  }else{
+    rank = "Học sinh giỏi";
+  }
+  return `${name} is ${age} years old\n` + `Average score: ${avg} -> ` + `${rank}\n` + `Best subject: ${strongestSubject} (${max})\n` + `Weakest subject: ${weakestSubject} (${min})`;
 };
-combine(arr1, arr2);
-arr3 = arr3.sort((a,b) => a - b);
-console.log(arr3);
+const student = {
+  student: {
+    name: "dev",
+    age: 20,
+    listMonhoc: [
+      { subject: "Math", score: 9 },
+      { subject: "English", score: 7.5 },
+      { subject: "Physics", score: 6 },
+      { subject: "Literature", score: 8.5 },
+    ],
+  },
+};
+console.log(getStudentSummary(student));
